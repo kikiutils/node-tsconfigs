d2a84d72a94957ca7818f50b56ce35c748d02742
kiki-kanri <a470666@gmail.com>
2025-03-10 12:01:18 +0800

    style: format scripts

--- build-and-publish.sh
+++ build-and-publish.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
+set -e
 cd "$(dirname "$(readlink -f "$0")")"
-pnpm run generate && cp ./bun.json ./LICENSE ./package.json ./README.md ./tsconfig.base.json ./dist && cd ./dist && npm publish
+pnpm run generate
+cp ./bun.json \
+    ./LICENSE \
+    ./package.json \
+    ./README.md \
+    ./tsconfig.base.json \
+    ./dist
+
+cd ./dist
+npm publish

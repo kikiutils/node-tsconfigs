249c4c8179f36b5c71e58bcfa09cc6cf4fc64477
kiki-kanri <a470666@gmail.com>
2026-08-06 21:38:52 +0800

    chore: update `modify-files-permissions.sh`

--- modify-files-permissions.sh
+++ modify-files-permissions.sh
@@ -8,17 +8,17 @@ REPOSITORY_ROOT="$(git -C "${SCRIPT_DIR}" rev-parse --show-toplevel)"
 git -C "${REPOSITORY_ROOT}" config --replace-all core.filemode true
 
 find "${REPOSITORY_ROOT}" \
-	\( -name .git -o -name node_modules -o -name target \) -prune -o \
-	-type f -exec chmod 600 {} +
+    \( -name .git -o -name node_modules -o -name target \) -prune -o \
+    -type f -exec chmod 600 {} +
 
 find "${REPOSITORY_ROOT}" \
-	\( -name .git -o -name node_modules -o -name target \) -prune -o \
-	-type d -exec chmod 700 {} +
+    \( -name .git -o -name node_modules -o -name target \) -prune -o \
+    -type d -exec chmod 700 {} +
 
 find "${REPOSITORY_ROOT}" \
-	\( -name .git -o -name node_modules -o -name target \) -prune -o \
-	\( -type f -path '*/bin/*' -exec chmod 700 {} + \)
+    \( -name .git -o -name node_modules -o -name target \) -prune -o \
+    \( -type f -path '*/bin/*' -exec chmod 700 {} + \)
 
 find "${REPOSITORY_ROOT}" \
-	\( -name .git -o -name node_modules -o -name target \) -prune -o \
-	\( -name '*.sh' -type f -exec chmod 700 {} + \)
+    \( -name .git -o -name node_modules -o -name target \) -prune -o \
+    \( -name '*.sh' -type f -exec chmod 700 {} + \)
